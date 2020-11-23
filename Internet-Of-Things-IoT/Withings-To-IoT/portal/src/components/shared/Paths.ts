6b99306a30cbb15758a841caa7ab02998b355a36
Alex Golesh <algolesh@microsoft.com>
2020-11-23 14:31:53 -0800

    Removing web client

--- Internet-Of-Things-IoT/Withings-To-IoT/portal/src/components/shared/Paths.ts
+++
@@ -1,7 +0,0 @@
-export enum Paths {
-  Home = "/",
-  DeviceConnect = "/connect/",
-  Dashboard = "/dashboard/",
-  BodyWeightChart = "/dashboard/body-weight",
-  BloodPressureChart = "/dashboard/blood-pressure",
-}
