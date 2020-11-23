dd04642577e15ce2b5b594b84005b9ca6a2fff73
Alex Golesh <74740177+alexgolesh-msft@users.noreply.github.com>
2020-11-23 14:51:17 -0800

    Update Constants.cs

--- Internet-Of-Things-IoT/Withings-To-IoT/mobile/Contoso.IoMT_Client/Contoso.IoMT_Client/Contoso.IoMT_Client/Constants.cs
+++ Internet-Of-Things-IoT/Withings-To-IoT/mobile/Contoso.IoMT_Client/Contoso.IoMT_Client/Contoso.IoMT_Client/Constants.cs
@@ -5,7 +5,7 @@ namespace Contoso.IoMT_Client
 {
     public static class Constants
     {
-        public const string ClientId = "1d8cac68-2480-41a2-8862-ddb1b860f857";
+        public const string ClientId = "[REPLACE_ME]";
         public const string IosKeychainSecurityGroups = "com.microsoft.adalcache";
 
         public static readonly string[] Scopes = { string.Empty };
@@ -14,31 +14,31 @@ namespace Contoso.IoMT_Client
 #pragma warning disable SA1401 // Fields should be private
         public static string WithingsAuthorizeUri = "https://account.withings.com/oauth2_user/authorize2";
         public static string WithingsTokenUri = "https://account.withings.com/oauth2/token";
-        public static string WithingsClientId = "5e88175faf8c4e241ab6706b502ca04c08b083b447f1a478fb1e1593d51d24be";
+        public static string WithingsClientId = "[REPLACE_ME]";
 
         public static string WithingsRedirectUri = $"{App.SCHEME}://theappp.com";
         public static string WithingsApiUri = "https://wbsapi.withings.net/";
         public static string WithingsScope = "user.info,user.metrics";
 
-        public static string H3BaseEndpoint = "https://h3devapi.azure-api.net/api/";
+        public static string H3BaseEndpoint = "[REPLACE_ME]";
         public static string H3UserEndpoint = "user";
         public static string H3ObservationsEndpoint = "observations";
         public static string WithingsAuthEndpoint = "withings/auth";
 
-        public static string AppCenterIOSSecret = "8508ca44-c3ba-4970-8d08-f69f24bb193d";
-        public static string AppCenterAndroidSecret = "eac53c73-76f4-4fa3-a2a8-6ed448d577b7";
+        public static string AppCenterIOSSecret = "[REPLACE_ME]";
+        public static string AppCenterAndroidSecret = "[REPLACE_ME]";
 #pragma warning restore SA1401 // Fields should be private
 
-        private const string TenantName = "h3dev";
-        private const string TenantId = "h3dev.onmicrosoft.com";
+        private const string TenantName = "[REPLACE_ME]";
+        private const string TenantId = "[REPLACE_ME]";
         private const string PolicySignin = "B2C_1_susi";
         private const string PolicyPassword = "B2C_1_reset";
 
         public static string NotificationChannelName { get; set; } = "Contoso_IoMT_NotifyChannel";
 
-        public static string NotificationHubName { get; set; } = "h3devapi";
+        public static string NotificationHubName { get; set; } = "[REPLACE_ME]";
 
-        public static string ListenConnectionString { get; set; } = "Endpoint=sb://h3devapinotifications.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=KXV3NwFD7llahtzI58iWRswoWev6zjddCf8bIIs640I=";
+        public static string ListenConnectionString { get; set; } = "[REPLACE_ME]";
 
         public static string DebugTag { get; set; } = "Contoso_IoMT_Notify";
 
